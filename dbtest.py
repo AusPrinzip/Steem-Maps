d05ca11fa7cc3c9fa6a21f9ee6e82b02e4ff1526
AusPrinzip <cdmdb@hotmail.com>
2017-10-28 19:49:15 +0200

    added static files, creation of data.json

---
+++ dbtest.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import datetime
+client = MongoClient('localhost', 27017)
+
+db = client.test_database
+
+ 
+post = {"author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()}
+
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+db.collection_names(include_system_collections=False)
--- steem_generalmap.py
+++ steem_generalmap.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify, render_template, request, Response
+from flask import Flask, jsonify, render_template, request, Response, url_for
 from geopy.geocoders import Nominatim
 from steemdata import SteemData
 from pymongo import MongoClient
+import dateutil.parser
+import json
+import os
+from time import sleep
 
 client = MongoClient('localhost', 27017)
 
@@ -9,47 +13,64 @@ geolocator = Nominatim(timeout=10)
 
 app = Flask(__name__)
 
+
+
+dateStr = "2017-10-27T12:27:12.000Z"
+myDatetime = dateutil.parser.parse(dateStr)
+
 s = SteemData()
 
-@app.route('/geolocation')
-def geolocation():
-
-	user = request.args.get('user', 0, type=str)
-	s = SteemData()
-
-	data = s.Accounts.aggregate(
-	[
+result = s.Accounts.aggregate(
+[
 {
-   "$match": {
+    "$match": {
             "profile.location": {"$exists": "true"},
-            "last_account_update": {"$gte": myDatetime}
-        }
+                            "last_account_update": {"$gte": myDatetime}
+                 }
     },
 
 
     { 
-    "$project":
-    {
-        "account":"$account",
-        "location":"$profile.location",
-        "_id":0
-    }},
-    {"$limit":30},
-	])
+        "$project":
+        {
+                "account":"$account",
+                "location":"$profile.location",
+                "_id":0
+            }},
+        #{"$limit":3},
+    ])
 
-for elemento in data:
-    # print(elemento["location"])
+
+# WE NEED TO CHECK IF AN ENTRY HAS BEEN GEOLOCATED ALREADY TO NOT OVERLOAD
+data = {}  
+data['people'] = [] 
+for elemento in result: 
     location = geolocator.geocode(elemento["location"])
+    sleep(1)
+    print("sleep 1 sec")
     if hasattr(location,'latitude'):
-    # elemento["location"] = location
-        print((elemento["account"], location,location.latitude, location.longitude))
+        data['people'].append({  
+            'user': elemento["account"],
+            'latitude': location.latitude,
+            'longitude': location.longitude
+        })
     else:
         print("no lat/lng attribute")
 
+filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), "./static/data.json")
 
-	return jsonify(result= str(user) + str("/") + str(location.longitude) + str("/") + str(location.latitude))
+with open(filename, 'w') as outfile: 
+    outfile.write("var data = ") 
+    json.dump(data, outfile)
+    
+
 
 
 @app.route('/')
 def index():
-    return render_template('index_simplemap.html')
+    url_for('static', filename='markerclusterer.js')
+    url_for('static', filename='data.json')
+    url_for('static', filename='static1.png')
+    url_for('static', filename='m2.png')
+    url_for('static', filename='m3.png')
+    return render_template('index_generalmap.html')
